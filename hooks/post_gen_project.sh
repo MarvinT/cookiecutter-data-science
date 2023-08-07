45c7341d3b295337748f72203673fcb4810ffb2b
Marvin <MarvinT@users.noreply.github.com>
2023-08-07 15:37:54 -0700

    add untracked files

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git init
+git add *
+git commit -m 'initial cookie cutter commit'
+# conda env create -f env.yaml
+# conda activate {{ cookiecutter.env_name }}
+# pre-commit install
+# pre-commit run --all-files
---
+++ {{ cookiecutter.repo_name }}/testing/test_environment.py
@@ -0,0 +1,7 @@
+def test_import():
+    import {{ cookiecutter.module_name }}
+
+
+if __name__ == "__main__":
+    test_import()
+    
